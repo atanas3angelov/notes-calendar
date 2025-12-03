28c4eb4ecb4a687eb03313f26148c38645ad2519
Atanas Angelov <atanas3angelov@gmail.com>
2025-12-03 17:50:01 +0200

    added notes functionality through a note board
    - refactored the string-like table into js-dom-like objects
    
    - turned the function that created the calendar into a class with methods
    - class benefits:
    -- more efficient management of calendar through js
    -- but mainly so that user can send callback to save updates on notes to db

--- notes-calendar.js
+++ notes-calendar.js
@@ -1,66 +1,243 @@
-function createCalendar(calendarId, year, month) {
+class NotesCalendar {
+  constructor(calendarId, year, month, notes) {
+    this.calendarDiv = document.getElementById(calendarId);
+    this.year = year;
+    this.month = month;
+    this.notes = notes;
+
+    this.calendarDiv.appendChild(this.createCalendarMenu(year, month));
+    this.calendarDiv.appendChild(this.createCalendar(year, month, notes));
+
+  }
+
+  createCalendarMenu(year, month) {
+    
+    const calendarMenu = document.createElement('div');
+    calendarMenu.setAttribute('id', 'notes-calendar-menu');
+
+    this.yearMenu = document.createElement('input');
+    this.yearMenu.setAttribute('id', 'notes-calendar-year');
+    this.yearMenu.type = 'number';
+    this.yearMenu.value = year;
+    this.yearMenu.onchange = () => {
+      console.log('year changed to ' + this.yearMenu.value);
+    };
+
+    this.monthMenu = document.createElement('input');
+    this.monthMenu.setAttribute('id', 'notes-calendar-month');
+    this.monthMenu.type = 'number';
+    this.monthMenu.value = month;
+    this.monthMenu.onchange = () => {
+      console.log('month changed to ' + this.monthMenu.value);
+    };
+
+    calendarMenu.append(this.yearMenu, this.monthMenu);
+
+    return calendarMenu;
+  }
+
+  createCalendar(year, month, notes) {
 
     let mon = month - 1;
     let d = new Date(year, mon);
     const weekDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    let week = getWeekNumber(d);
+    let week = this.getWeekNumber(d);
 
-    let table = '<table>';
+    const table = document.createElement('table');
 
-    table += '<tr><th class="wk">w</th>';
+    let tr = document.createElement('tr');
+
+    let th = document.createElement('th');
+    th.classList.add('wk');
+    th.textContent = 'w';
+    tr.appendChild(th);
+
     for (let i = 0; i < weekDay.length; i++) {
-      table += '<th>' + weekDay[i] + '</th>';
+      th = document.createElement('th');
+      th.textContent = weekDay[i];
+      tr.appendChild(th);
     }
-    table += '</tr>';
+
+    table.appendChild(tr);
 
     // outer loop to add rows by weeks
     while (d.getMonth() == mon) {
       
-      table += '<tr><td class="wk">' + week + '</td>'
+      tr = document.createElement('tr');
 
+      let td = document.createElement('td');
+      td.classList.add('wk');
+      td.textContent = week;
+      tr.appendChild(td);
+      
       // add spaces before first day of month (for the first row)
       // from Monday till the first day of the month
       // * * * 1  2  3  4
-      for (let i = 0; i < getDay(d); i++) {
-        table += '<td></td>';
+      for (let i = 0; i < this.getDay(d); i++) {
+        tr.appendChild(document.createElement('td'));
       }
 
-      for (let i = getDay(d); i < 7; i++) {
-        table += '<td>' + d.getDate() + '</td>';
+      for (let i = this.getDay(d); i < 7; i++) {
+
+        td = document.createElement('td');
+
+        if (notes[d.getDate()]) {
+          td.classList.add('noted');
+          td.title = notes[d.getDate()][0].join(', ');
+        }
+
+        td.textContent = d.getDate();
+        td.onclick = ((day) => (e) => this.showNotes(e, day))(d.getDate());
+
+        tr.appendChild(td);
+        
         d.setDate(d.getDate() + 1);
 
         // add spaces after last day of month (for the last row)
         // 29 30 31 * * * *
         if ((d.getMonth() != mon) ){
           for (let j = i + 1; j < 7; j++) {
-            table += '<td></td>';
+            tr.appendChild(document.createElement('td'));
           }
           break;
         }
+        table.appendChild(tr);
       }
 
-      table += '</tr>';
       week += 1;
     }
 
-    // close the table
-    table += '</table>';
+    return table;
+  }
 
-    document.getElementById(calendarId).innerHTML = table;
-}
-
-function getDay(date) { // get day number from 0 (monday) to 6 (sunday)
+  getDay(date) { // get day number from 0 (monday) to 6 (sunday)
     let day = date.getDay();
     if (day == 0) day = 7; // make Sunday (0) the last day
     return day - 1;
-}
+  }
 
-const getWeekNumber = (date) => {
+  getWeekNumber = (date) => {
     const firstDay = new Date(date.getFullYear(), 0, 1)
     const days = Math.floor((date - firstDay) / (24 * 60 * 60 * 1000))
-    return Math.ceil((days + getDay(firstDay) + 1) / 7)
+    return Math.ceil((days + this.getDay(firstDay) + 1) / 7)
+  }
+
+  createNoteBoard(noteboardId) {
+    this.noteboard = document.getElementById(noteboardId);
+
+    this.noteboardDay = 1;
+
+    this.summary = document.createElement('input');
+    this.summary.setAttribute('id','note-board-summary');
+
+    const infoButton = document.createElement('button');
+    infoButton.setAttribute('id', 'note-board-info-button');
+    infoButton.innerText = 'i';
+    infoButton.onclick = () => this.toggleDetails();
+
+    const editButton = document.createElement('button');
+    editButton.setAttribute('id', 'note-board-edit-button');
+    editButton.innerText = 's';
+    editButton.onclick = () => this.editNotes();
+
+    this.details = document.createElement('textarea');
+    this.details.setAttribute('id', 'note-board-details');
+
+    this.noteboard.append(this.summary, infoButton, editButton, this.details);
+  }
+
+  showNotes(e, day) {
+
+    if (this.selectedCell)
+      this.selectedCell.classList.remove('selected-day');
+
+    this.selectedCell = e.target;
+    this.selectedCell.classList.add('selected-day');
+
+    this.noteboardDay = day;
+
+    if (this.notes[day]) {
+      this.summary.value = this.notes[day][0].join(', ');
+      this.details.value = this.notes[day][1];
+    }
+    else {
+      this.summary.value = '';
+      this.details.value = '';
+    }
+  }
+
+  toggleDetails() {
+
+    if (this.details) // if user defined where the noteboard should be placed
+      this.details.style.display = window.getComputedStyle(this.details).display == 'inline-block' ? 
+        'none' : 'inline-block';
+    
+    // else add it as an overlay on the calendar ...TO DO
+  }
+
+  editNotes() {
+    
+    const day = this.noteboardDay;
+
+    const summary = this.summary.value;
+
+    const details = this.details.value;
+
+    this.saveNote(2025, 12, day, summary.split(',').map((e) => e.trim()), details);
+  }
+
+  saveNote(year, month, day, summary, details) {
+
+    notes[day] = [summary, details];
+
+    if (summary.join(', ') == '' && details == '') {
+      this.selectedCell.classList.remove('noted');
+      delete notes[day];
+    } else {
+      this.selectedCell.classList.add('noted');
+    }
+    
+    this.selectedCell.title = summary.join(', ');
+
+    this.saveCallback(year, month, day, summary, details);
+  }
+
+  defineCallbackSave(func) {
+    this.saveCallback = func;
+  }
+
 }
 
 window.onload = function() {
-    createCalendar('notes-calendar', 2025, 12);
+  
+  // notes depicting revision/learning of a skill (e.g. Golang)
+  notes = {
+    /* day number: [['tag/summary 1', 'tag/summary 2'...], 'details as text'] */
+    3:  [['go'], 'A Tour of Go: variables & functions'],
+    4:  [['go'], 'A Tour of Go: flow control - for'],
+    5:  [['go', 'python'], 'A Tour of Go: flow control - if, else, switch, defer\n' + 
+                            'python - enumerate\n' + 
+                            'c# closures\n' + 
+                            'java enhanced for loop'],
+    6:  [['go'], 'A Tour of Go: structs'],
+    10: [['go'], 'A Tour of Go: slices and maps'],
+    11: [['go'], 'A Tour of Go: exercise'],
+    12: [['go'], ''],
+    17: [['go'], ''],
+    18: [['go'], 'A Tour of Go: '],
+    20: [['js', 'css', 'html'], 'creating a calendar with js'],
+    25: [['css'], 'css universal selector and combinators'],
+    26: [['js, html'], 'figuring out how to display notes for the note calendar project']
+  }  // data to be collected from a database with a backend call
+
+  const notesCalendar = new NotesCalendar('notes-calendar', 2025, 12, notes);
+
+  notesCalendar.createNoteBoard('note-board');
+
+  const callbackSave = (year, month, day, summary, details) => {
+    notes[day] = [[summary], details];
+    console.log(notes);
+  };
+
+  notesCalendar.defineCallbackSave(callbackSave);
 };

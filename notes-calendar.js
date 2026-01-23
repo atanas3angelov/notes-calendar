85e73720e81c4898c0fff6397f504201b6b6bb37
Atanas Angelov <atanas3angelov@gmail.com>
2026-01-23 22:07:39 +0200

    implemented overlay noteboard
    - added 2nd preview table (with overlay noteboard) side-by-side with original
    
    - improved error handling from user input
    
    - refactored some unnecessary method params

--- notes-calendar.js
+++ notes-calendar.js
@@ -5,18 +5,37 @@ class NotesCalendar {
      * @param {string} calendarId The uniqure html id used to position the calendar object.
      * @param {number} year The initial year the calendar should display.
      * @param {number} month The initial month the calendar should display.
-     * @param {Object} notes Data in format {number: {summary: Array.<string>, details: string}}.
+     * @param {Object} notes Data in format {number: {summary: Array.<string>, details: string}, ...}.
      */
     this.calendarDiv = document.getElementById(calendarId);
-    this.year = year;
-    this.month = month;
-    this.notes = notes;
+    if (this.calendarDiv) {
 
-    this.calendarDiv.appendChild(this.createCalendarMenu(year, month));
-    this.calendarDiv.appendChild(this.createCalendar(year, month, notes));
+      this.overlayBoard = true; // if user doesn't define a notesboard, an overlaid one need be created
+      this.detailsVisible = true;
+      this.autoresizeBoard = true;  // influences only static board, overlay is always autoresizable
+
+      if (isNaN(year)) {
+        console.error(`Expecting year '${year}' to be a number.`);
+
+      } else if (isNaN(month) || month < 1 || month > 12) {
+        console.error(`Expecting month '${month}' to be a number from 1 to 12.`);
+        
+      } else {
+        this.year = year;
+        this.month = month;
+        this.notes = notes;
+
+        this.calendarDiv.appendChild(this.createCalendarMenu());
+        this.calendarDiv.appendChild(this.createCalendar());
+      }
+
+    } else {
+      console.error(`Missing id '${calendarId}' for note board.`);
+    }
+
   }
 
-  createCalendarMenu(year, month) {
+  createCalendarMenu() {
     
     const calendarMenu = document.createElement('div');
     calendarMenu.classList.add('notes-calendar-menu');
@@ -24,7 +43,7 @@ class NotesCalendar {
     this.yearMenu = document.createElement('input');
     this.yearMenu.classList.add('notes-calendar-year');
     this.yearMenu.type = 'number';
-    this.yearMenu.value = year;
+    this.yearMenu.value = this.year;
     this.yearMenu.onchange = () => {
       this.year = this.yearMenu.value;
       this.loadCallback(this.year, this.month);
@@ -33,7 +52,7 @@ class NotesCalendar {
     this.monthMenu = document.createElement('input');
     this.monthMenu.classList.add('notes-calendar-month');
     this.monthMenu.type = 'number';
-    this.monthMenu.value = month;
+    this.monthMenu.value = this.month;
     this.monthMenu.onchange = (event) => {
       if (event.target.value > 12) {
         this.yearMenu.value = Number(this.yearMenu.value) + 1;
@@ -54,10 +73,10 @@ class NotesCalendar {
     return calendarMenu;
   }
 
-  createCalendar(year, month, notes) {
+  createCalendar() {
 
-    let mon = month - 1;
-    let d = new Date(year, mon);
+    let mon = this.month - 1;
+    let d = new Date(this.year, mon);
     const weekDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     let week = this.getWeekNumber(d);
 
@@ -99,9 +118,9 @@ class NotesCalendar {
 
         td = document.createElement('td');
 
-        if (notes[d.getDate()]) {
+        if (this.notes[d.getDate()]) {
           td.classList.add('noted');
-          td.title = notes[d.getDate()].summary.join(', ');
+          td.title = this.notes[d.getDate()].summary.join(', ');
         }
 
         td.textContent = d.getDate();
@@ -140,13 +159,33 @@ class NotesCalendar {
     return Math.ceil((days + this.getDay(firstDay) + 1) / 7)
   }
 
-  createNoteBoard(noteboardId) {
+  createNoteBoard(noteboardId, autoresize = true) {
+    /** This func will use the provided id to place a static noteboard. If the provided id is not in 
+     * the document, the calendar will still fall back to an overlay one.
+     * @param {string} noteboardId element id, where the static noteboard should be placed.
+     * @param {boolean} autoresize determines whether to autoresize static noteboard, default: true
+     */
     this.noteboard = document.getElementById(noteboardId);
+    this.autoresizeBoard = autoresize;
 
+    if (this.noteboard) {
+      this.overlayBoard = false;
+
+      this.createBoardElements();
+
+    } else {
+      console.warn(`Missing id '${noteboardId}' for note board. Falling back to an overlay one.`);
+    }
+  }
+
+  createBoardElements() {
     this.noteboardDay = 1;
 
     this.summary = document.createElement('input');
     this.summary.classList.add('note-board-summary');
+
+    if (this.autoresizeBoard || this.overlayBoard)
+      this.summary.oninput = () => this.adjustBoardWidth();
 
     const infoButton = document.createElement('button');
     infoButton.classList.add('note-board-info-button');
@@ -161,7 +200,48 @@ class NotesCalendar {
     this.details = document.createElement('textarea');
     this.details.classList.add('note-board-details');
 
+    if (this.autoresizeBoard || this.overlayBoard)
+      this.details.oninput = () => this.adjustBoardWidth();
+
     this.noteboard.append(this.summary, infoButton, editButton, this.details);
+  }
+
+  adjustBoardWidth() {
+    let summaryWidth = (this.summary.value.length + 1) * 8;
+
+    let detailsWidth = 0;
+    if (this.detailsVisible) {
+      let text = this.details.value;
+      let lineText = text.split(/\n/);  // wrapping lines can be an issue
+      let lines = lineText.length;
+      this.details.rows = lines;
+      detailsWidth = Math.max(...lineText.map(txt => txt.length)) * 8;
+    }
+
+    if (summaryWidth + 40 > detailsWidth) { // i, s buttons 20px each
+      this.summary.style.width = summaryWidth + "px";
+      this.noteboard.style.width = summaryWidth + 40 + "px";  // i, s buttons 20px each
+    } else {
+      this.noteboard.style.width = detailsWidth + "px";
+      this.summary.style.width = detailsWidth - 40 + "px";  // i, s buttons 20px each
+    }
+
+  }
+
+  createOverlayBoard() {
+    this.noteboard = document.createElement('div');
+    this.noteboard.classList.add('note-board-overlay');
+    
+    this.noteboard.style.display = 'block';
+    this.noteboard.style.position = 'absolute';
+
+    document.body.append(this.noteboard);
+
+    this.createBoardElements();
+
+    this.toggleDetails();
+
+    this.noteboard.onmouseleave = () => this.noteboard.style.display = 'none';
   }
 
   showNotes(e, day) {
@@ -171,6 +251,23 @@ class NotesCalendar {
 
     this.selectedCell = e.target;
     this.selectedCell.classList.add('selected-day');
+
+    // user did not provide a static noteboard, create an overlay one
+    if (this.overlayBoard) {
+
+      let boundingRect = e.target.getBoundingClientRect();
+
+      if (this.noteboard) {
+        this.noteboard.style.display = 'block';
+        
+      } else {
+        this.createOverlayBoard();
+      }
+
+      // position bellow element from which event was initiated
+      this.noteboard.style.top = boundingRect.bottom + 'px';
+      this.noteboard.style.left = boundingRect.left + 'px';
+    }
 
     this.noteboardDay = day;
 
@@ -182,15 +279,19 @@ class NotesCalendar {
       this.summary.value = '';
       this.details.value = '';
     }
+
+    if (this.autoresizeBoard || this.overlayBoard)
+      this.adjustBoardWidth();
   }
 
   toggleDetails() {
-
-    if (this.details) // if user defined where the noteboard should be placed
+    if (this.details)
       this.details.style.display = window.getComputedStyle(this.details).display == 'inline-block' ? 
         'none' : 'inline-block';
-    
-    // else add it as an overlay on the calendar ...TO DO
+      this.detailsVisible = this.details.style.display == 'none' ? false : true;
+
+      if (this.autoresizeBoard || this.overlayBoard)
+        this.adjustBoardWidth();
   }
 
   editNotes() {
@@ -201,6 +302,10 @@ class NotesCalendar {
     const summaryTags = summary.split(',').map((e) => e.trim());
 
     const details = this.details.value;
+
+    if (this.overlayBoard) {
+      this.noteboard.style.display = 'none';
+    }
 
     this.saveNote(this.year, this.month, day, summaryTags, details);
   }
@@ -227,7 +332,9 @@ class NotesCalendar {
      * @param {Object} notes Data in format {number: {summary: Array.<string>, details: string}}.
      */
     this.notes = notes;
-    this.calendarDiv.replaceChild(this.createCalendar(this.year, this.month, this.notes), this.calendarDiv.children[1]);
+    this.calendarDiv.replaceChild(
+      this.createCalendar(this.year, this.month, this.notes), 
+      this.calendarDiv.children[1]);
   }
 
   /**
